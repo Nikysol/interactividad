fc6bfdfb4cd2eebf13e5f15291a9c1e938cbe7ee
nicole <nikysol@icloud.com>
2025-10-08 09:43:03 -0600

    variables y funciones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+let nombre = "Nicole";
+let edad = 21;
+let esMayorDeEdad = edad >= 18
+const TAU = 3.1416;
+
+edad = 22;
+console.log(`Hola ${nombre} tienes ${edad} años`);
+
+function saludar(nombre="Mundo") {
+    alert(`Hola ${nombre}`);
+}
+
+saludar();
+saludar(nombre);
+saludar("Nicole");
+saludar("Juan");
